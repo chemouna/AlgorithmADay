76bd960265822ea5c414bcd01e189606aa0605ed
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-03-10 09:14:17 +0100

    Counting sort test.

---
+++ JavaAlgoADay/test/com/mounacheikhna/algoday/CountingSortTest.java
@@ -0,0 +1,28 @@
+package com.mounacheikhna.algoday;
+
+import java.util.Arrays;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by m.cheikhna on 09/03/2017.
+ */
+public class CountingSortTest {
+
+    private CountingSort cs;
+
+    @Before
+    public void setUp() throws Exception {
+        cs = new CountingSort();
+    }
+
+    @Test
+    public void countingSortSortsArray() throws Exception {
+        int [] a = new int[] {1, 5, 2, 7, 3, 9, 4, 6, 8, 1};
+        cs.countingSort(a);
+        System.out.println(Arrays.toString(a));
+        Assert.assertArrayEquals(new int[]{1, 1, 2, 3, 4, 5, 6, 7, 8, 9}, a);
+    }
+
+}
